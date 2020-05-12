05f6728f0e7f74fa98f3cd4be46fc105270216b2
Max Tanous <mtanous@marketron.com>
2020-05-12 13:23:58 -0600

    Added influence code, vizualizations, gephi files, and reports

--- Graph.py
+++ Graph.py
@@ -2,7 +2,40 @@ import networkx as nx
 from Cities import Cities
 import matplotlib.pyplot as plt
 import random
+import numpy as np
+import time
 
+NODE_CITY_DICT = {
+    'Boise': 0,
+    'Twin  Falls':1,
+    'Idaho Falls':2,
+    'Rexburg':3,
+    'Pocotello': 4,
+    "Couer d'Alene":5,
+    'Hailey':6,
+    "Ketchum":7,
+    "Sun Valley":8,
+    "MCALL": 9,
+    "Nampa": 10,
+    "Meridian": 11,
+    "Lewiston": 12
+}
+
+THRESHOLD_DICT = {
+    'Boise': 0.335,
+    'Twin  Falls': .431,
+    'Idaho Falls': .392,
+    'Rexburg': .431,
+    'Pocotello': .0751,
+    "Couer d'Alene": .4214,
+    'Hailey': .3783,
+    "Ketchum": .5382,
+    "Sun Valley":.3708,
+    "MCALL": .4322,
+    "Nampa": .1678,
+    "Meridian": .4251,
+    "Lewiston": .2981,
+}
 class Graph:
 
     def __init__(self):
@@ -12,6 +45,7 @@ class Graph:
         self.edges = {}
         self.edge_map = {}
         self.init_from_city_data()
+        # Change ot build_graph_ic if running influence maximization 
         self.build_graph()
 
     def init_from_city_data(self):
@@ -19,17 +53,36 @@ class Graph:
         self.cities = cities.cities
         self.edge_map = cities.city_neighbors
 
+    def build_graph_ic(self):
+        self.network = nx.Graph()
+        counter = 0 
+        for city in self.cities:
+            self.network.add_node(counter, threshold=THRESHOLD_DICT[city] + .2, active=False)
+            counter += 1
+        edge_map = self.edge_map
+        for edge in edge_map:
+            for key, value in edge.items():
+                key_node = NODE_CITY_DICT[key]
+                print(key)
+                print(key_node)
+                
+                for node in value:
+                    self.network.add_edge(key_node,NODE_CITY_DICT[node])
+
     
     def build_graph(self):
         self.network = nx.Graph()
+        counter = 0 
         for city in self.cities:
-            print('Random ', city, random.uniform(0,0.4))
-
-            self.network.add_node(city)
+            print(city)
+            self.network.add_node(counter)
+            counter += 1
         edge_map = self.edge_map
         for edge in edge_map:
             for key, value in edge.items():
                 key_node = key
+                print(key)
+                print(value)
                 for node in value:
                     self.network.add_edge(key_node,node)
 
@@ -37,8 +90,96 @@ class Graph:
         nx.draw(self.network, with_labels=True)
         print(self.network.edges)
         plt.show()
+
+    def IC(self,S,p,mc):
+    
+        self.network
+        # Loop over the Monte-Carlo Simulations
+        spread = []
+        for i in range(mc):
+            
+            # Simulate propagation process      
+            new_active, A = S[:], S[:]
+            while new_active:
+                # 1. Find out-neighbors for each newly active node
+                targets = self.propagate_nx(p,new_active)
+    
+                #print('targets', targets)
+                # 2. Determine newly activated neighbors (set seed and sort for consistency)
+                np.random.seed(i)
+                success =[]
+                for node in targets:
+                    success.append(self.activate_node(node))
+                # success = np.random.uniform(0,1,len(targets)) < p
+                
+                new_ones = list(np.extract(success, sorted(targets)))
+                # 3. Find newly activated nodes and add to the set of activated nodes
+                new_active = list(set(new_ones) - set(A))
+                A += new_active
+                
+            spread.append(len(A))
+            
+        return np.mean(spread)
+
+
+    def greedy(self,k,p=0.1,mc=1000):
+        """
+        Input:  graph object, number of seed nodes
+        Output: optimal seed set, resulting spread, time for each iteration
+        """
+        S, spread, timelapse, start_time = [], [], [], time.time()
+        g = self.network
+        # Find k nodes with largest marginal gain
+        for _ in range(k):
+
+            # Loop over nodes that are not yet in seed set to find biggest marginal gain
+            best_spread = 0
+            for j in set(range(len(g.nodes())))-set(S):
+                nx.set_node_attributes(self.network, False, 'active')
+                for node in S + [j]:
+                    self.network.nodes[node]['active'] = True
+                # Get the spread
+                s = self.IC(S + [j],p,mc)
+                print('spread: ', s)
+                print('set: ', S + [j])
+                # Update the winning node and spread so far
+                if s > best_spread:
+                    best_spread, node = s, j
+
+            # Add the selected node to the seed set
+            S.append(node)
+            
+            # Add estimated spread and elapsed time
+            spread.append(best_spread)
+            timelapse.append(time.time() - start_time)
+        return(S,spread,timelapse)
+
+    def propagate_nx(self,p,new_active):
+        targets = []
+        for node in new_active:
+            targets += self.network.neighbors(node)
+        return(targets)
+
+
+
+
+    def activate_node(self, node):
+        threshold = self.network.nodes[node]['threshold']
+        active_neighbors = 0
+        neighbors = self.network.neighbors(node)
+        total_nieghbors = 0
+        for neighbor in neighbors: 
+            total_nieghbors +=1
+            if self.network.nodes[neighbor]['active']:
+                active_neighbors += 1
+        if active_neighbors == 0:
+            return False
+        elif active_neighbors/total_nieghbors > threshold:
+            self.network.nodes[node]['active'] = True
+            return True
+        else:
+            return False
             
         
 
-#graph = Graph()
-# graph.visualize_graph()
+
--- InfluenceEnergyProblem.py
+++ InfluenceEnergyProblem.py
@@ -1,9 +1,9 @@
 from Sentiment import Sentiment
 from Graph import Graph
-SENTIMENT_CITIES = ['boise', 'twin_falls', 'meridian', 'nampa', 'sun_valley', 'idaho_falls', 'rexburg', 'coeur_dalene', 'hailey', 'lewiston']
+#SENTIMENT_CITIES = ['boise', 'twin_falls', 'meridian', 'nampa', 'sun_valley', 'idaho_falls', 'rexburg', 'coeur_dalene', 'hailey', 'lewiston']
 
 ### FOR LEGISLSATION 
-#SENTIMENT_CITIES = ['boise', 'twin_falls', 'meridian', 'sun_valley', 'idaho_falls', 'coeur_dalene', 'lewiston']
+SENTIMENT_CITIES = ['boise', 'twin_falls', 'meridian', 'sun_valley', 'idaho_falls', 'coeur_dalene', 'lewiston']
 class InfluenceEnergyProblem: 
 
     def __init__(self, to_tokenize, doc_type):
@@ -21,5 +21,14 @@ class InfluenceEnergyProblem:
             city_sentiment = Sentiment(city, self.to_tokenize, self.document_type)
             city_sentiment.get_word_frequency('biomass')
 
-prob = InfluenceEnergyProblem(True, 'articles')
-prob.get_city_thresholds()
+    def get_corpus_size(self):
+        total_number_of_docs = 0
+        for city in self.senitiment_cities:
+            city_sentiment = Sentiment(city, self.to_tokenize, self.document_type)
+            total_number_of_docs += len(city_sentiment.filenames)
+        print(total_number_of_docs)
+
+    def activate_node(self):
+        
+prob = InfluenceEnergyProblem(True, 'legislation')
+prob.get_corpus_size()
